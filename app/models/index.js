da74e83d193b784293d512674edbc9481ef20396
faktaarief <faktaarief16@gmail.com>
2021-05-24 05:32:26 +0700

    Initialize Sequelize

---
+++ app/models/index.js
@@ -0,0 +1,19 @@
+const dbConfig = require('../config/db.config')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorAliases: false,
+
+  pool: {
+    max: dbConfig.max,
+    min: dbConfig.min,
+    acquire: dbConfig.acquire,
+    idle: dbConfig.idle
+  }
+})
+
+const db = {}
+
+module.exports = db
