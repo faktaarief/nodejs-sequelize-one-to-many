d23d72930e2c2475b556480cc6eea2ef74a1268d
faktaarief <faktaarief16@gmail.com>
2021-05-24 07:42:03 +0700

    Added More Method in Tutorial Controller

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -16,3 +16,50 @@ exports.createTutorial = (tutorial) => {
       console.log(`>> Error while creating tutorial ${err}`)
     })
 }
+
+// Create and Save New Comments
+exports.createComment = (tutorial_id, comment) => {
+  return Comment.create({
+    name: comment.name,
+    text: comment.text,
+    tutorial_id: tutorial_id
+  })
+    .then(comment => {
+      console.log(`>> Created comment: ${JSON.stringify(comment, null, 4)}`)
+      return comment
+    })
+    .catch(err => {
+      console.log(`>> Error while creating comment: ${err}`)
+    })
+}
+
+// Get The Comment For A Given Tutorial
+exports.findTutorialById = (tutorial_id) => {
+  return Tutorial.findByPk(tutorial_id, { include: ['comments'] })
+    .then(tutorial => {
+      return tutorial
+    })
+    .catch(err => {
+      console.log(`>> Error while finding tutorial: ${err}`)
+    })
+}
+
+// Get The Comments For A Given Comment ID
+exports.findCommentById = (id) => {
+  return Comment.findByPk(id, { include: ['tutorial'] })
+    .then(comment => {
+      return comment
+    })
+    .catch(err => {
+      console.log(`>> Error while finding comment: ${err}`)
+    })
+}
+
+// Get All Tutorials Include Comments
+exports.findAll = () => {
+  return Tutorial.findAll({
+    include: ['comments']
+  }).then(tutorials => {
+    return tutorials
+  })
+}
